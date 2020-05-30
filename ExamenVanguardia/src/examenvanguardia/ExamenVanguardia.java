c477625c24dcacb27825bc5b7887c55c85c84ac7
carlosvhz <carlosvhz@unitec.edu>
2020-05-30 16:40:56 -0600

    Observer

--- ExamenVanguardia/src/examenvanguardia/ExamenVanguardia.java
+++ ExamenVanguardia/src/examenvanguardia/ExamenVanguardia.java
@@ -11,10 +11,22 @@ public class ExamenVanguardia {
         singleton.writeFile("Holliwis");*/
         
         // Factory
-        TableFactory factory = new TableFactory();
+        /*TableFactory factory = new TableFactory();
         Factory table1 = factory.createTable("MySQL");
         table1.doSomething();
         table1 = factory.createTable("Oracle");
-        table1.doSomething();
+        table1.doSomething();*/
+        
+        UserSubject subject = new UserSubject();
+        
+        UserObserver user1 = new UserObserver(subject, "Mike");
+        UserObserver user2 = new UserObserver(subject, "Elise");
+        UserObserver user3 = new UserObserver(subject, "Poncho");
+        UserObserver user4 = new UserObserver(subject, "Fredo");
+        
+        user1.suggest(10);
+        user2.suggest(11);
+        user3.suggest(156);
+        user4.suggest(10000);
     }
 }
---
+++ ExamenVanguardia/src/examenvanguardia/Observer.java
@@ -0,0 +1,62 @@
+package examenvanguardia;
+
+import java.util.ArrayList;
+
+/**
+ * Se toma coomo caso el uso de este patron de diseño en un sistema de ventas como Ebay
+ */
+public abstract class Observer {   
+    protected UserSubject user;
+    public abstract void update();
+}
+
+class UserSubject {
+    private ArrayList<Observer> observers = new ArrayList<Observer>();
+    private int amount;
+    
+    public int getAmount(){
+        return amount;
+    }
+    
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+    
+    public void registerObserver(Observer observer){
+        observers.add(observer);
+    }
+    
+    public void notifyAllObservers(){
+        for (int i = 0; i < observers.size(); i++) {
+            observers.get(i).update();
+        }
+    }
+}
+
+class UserObserver extends Observer{
+    
+    private String name;
+    public UserObserver(UserSubject user, String name){
+        this.user = user;
+        this.name = name;
+        this.user.registerObserver(this);
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void suggest(int amount){
+        if(amount > user.getAmount()){
+            System.out.println("El usuario "+name+" ha propuesto "+amount+" lempiras");
+            user.setAmount(amount);
+        }else{
+            System.out.println("No puedes agregar menos del dinero ya propuesto!!");
+        }
+    }
+    
+    @Override
+    public void update(){
+        System.out.println("nuevo monto es: "+user.getAmount());
+    }
+}
